0e58e3c192c3f18dc0417230702bc3df8eef37b8
RafayMemon01 <rafayrashid200611@gmail.com>
2025-02-08 17:57:20 +0500

    Add initial HTML and JavaScript files for variable demonstration

---
+++ JS/01 JS Variables/script.js
@@ -0,0 +1,17 @@
+// let a = "Hello"; //"" = string
+
+// // alert(a);
+
+// // alert("Hello This is Alert Box")
+
+// let b = a;
+
+// let num1 = "4";
+// let num2 = "4";
+
+// console.log(num1 + num2);
+
+let userName = "Salman";
+console.log(userName)
+userName = "Salman Farooq";
+console.log(userName)
