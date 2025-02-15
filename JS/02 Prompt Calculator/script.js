e895c7022efa679f126fcbe0f3b0f55da6bf724e
RafayMemon01 <rafayrashid200611@gmail.com>
2025-02-15 18:10:43 +0500

    Add HTML and JavaScript files for addition and subtraction calculators

---
+++ JS/02 Prompt Calculator/script.js
@@ -0,0 +1,9 @@
+// let num1 = prompt("Enter Your First Number")
+// let num2 = prompt("Enter Your Second Number")
+
+// num1 = Number(num1)
+// num2 = Number(num2)
+
+// let result = num1*num2
+
+// alert(result)
