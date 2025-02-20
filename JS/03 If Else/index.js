3c04f776c4ca7169b7b8326baa624913ca1a6ab5
RafayMemon01 <rafayrashid200611@gmail.com>
2025-02-20 17:45:54 +0500

    if else

---
+++ JS/03 If Else/index.js
@@ -0,0 +1,14 @@
+let a = "apple";
+let b = 123;
+
+console.log(a ,b);
+
+let userName = "Jhon Doe";
+
+if(userName === "Jhon Doe"){
+    console.log("Hello Jhone Doe");
+
+}else{
+    console.log("Undefined User");
+   
+}
---
+++ JS/04 Dummy Weather Suggestion/script.js
@@ -0,0 +1,7 @@
+let userCity = prompt("Write Your City Nam eHere")
+
+if(userCity === "karachi"){
+    alert("Today weather is Good")
+}else{
+    alert("We have not enogh data for you city")
+}
