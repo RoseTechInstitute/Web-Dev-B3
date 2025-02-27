0e5c9dfca8ab525c109c9751e01e138a2d3e2d72
RafayMemon01 <rafayrashid200611@gmail.com>
2025-02-27 17:59:22 +0500

    unfimilar operators

--- JS/04 Dummy Weather Suggestion/script.js
+++ JS/04 Dummy Weather Suggestion/script.js
@@ -1,7 +1,18 @@
 let userCity = prompt("Write Your City Nam eHere")
 
+// if(userCity === "karachi"){
+//     alert("Today weather is Good")
+// }else{
+//     alert("We have not enogh data for your city")
+// }
+
 if(userCity === "karachi"){
-    alert("Today weather is Good")
+    alert("Today weather is Good");
+}else if(userCity === "lahore"){
+    alert("Today weather is foggy")
+}else if(userCity === "hydrabad"){
+    alert("Today weather is hot")
 }else{
-    alert("We have not enogh data for you city")
+        alert("We have not enogh data for your city")
+
 }
---
+++ JS/05 UnfimiliarOperators/index.js
@@ -0,0 +1,12 @@
+let num1 = 20; //20+1 = 21 /22/
+
+//POST Increament
+
+// console.log(num1++)
+// console.log(num1++)
+// console.log(num1)
+
+//PRE Increament
+
+console.log(++num1)
+
