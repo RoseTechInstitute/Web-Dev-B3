2cc5f12b503bf8c0ae871c9a0402b7adc38c4c89
RafayMemon01 <rafayrashid200611@gmail.com>
2025-04-12 18:26:40 +0500

    Add initial HTML and JavaScript files for fruit array manipulation

---
+++ JS/07 Array/index.js
@@ -0,0 +1,12 @@
+let fruits = ["Apple","Banana","Mango", "Orange"]
+
+
+// console.log(fruits[1])
+
+fruits[4] = "Grapes"
+
+fruits.push("Pineapple","Watermelon")
+fruits.pop()
+fruits.shift()
+fruits.unshift("Kiwi")
+console.log(fruits)
